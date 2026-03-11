685594db4353366faa0cc8d9c464e43f454d4c43
cd14c <a01742620@tec.mx>
2026-03-11 09:13:53 -0600

    RoutingWorkDemo

--- quizTime/src/App.tsx
+++ quizTime/src/App.tsx
@@ -1,26 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./pages/Navbar/Navbar";
-import Form from "./pages/mainPages/Form";
-import About from "./pages/mainPages/About";
-import "./App.css";
-
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
   return (
-    <Router>
-      <div className="app">
-        {/* Barra de navegación */}
-        <Navbar />
-        {/* Contenido de la página */}
-        <div className="content">
-          <Routes>
-            <Route path="/about" element={<About />} />
-            <Route path="/form" element={<Form />} />
-          </Routes>
-        </div>
-      </div>
-    </Router>
-  );
+    <div className='header'>
+      <h1>GRAVITY FALLS QUIZ</h1>
+
+    </div>
+  )
 }
 
 export default App;
