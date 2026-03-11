2c60bcec2f0316be36301811cd6cd4d05ff4e028
Hannia Escamilla Pérez <hanescper@gmail.com>
2026-03-11 08:43:26 -0600

    NavBar

--- quizTime/src/App.tsx
+++ quizTime/src/App.tsx
@@ -1,15 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import NavBar from './Page/NavBar/NavBar.tsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='header'>
-      <h1>GRAVITY FALLS QUIZ</h1>
-
+    <div>
+      <NavBar />
     </div>
   )
 }
---
+++ quizTime/src/Page/NavBar/NavBar.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './NavBar.css'
+
+function NavBar(){
+    return(
+        <header className='header'>
+            <p className='hStyle'>GRAVITY FALLS QUIZ</p>
+            <p className='pStyle'>About</p>
+
+        </header>
+    )
+}
+
+export default NavBar;
